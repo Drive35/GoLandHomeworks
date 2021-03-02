46104c3ab81bfa68cac638a5d8e7474819aaa3a0
Drive35 <zhomart.zhanuzakov@gmail.com>
2021-03-02 22:42:01 +0600

    hw5

--- homeworks/hw5_2nd_task.go
+++ homeworks/hw5_2nd_task.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func main() {
+	//2) найти сумму положительных элементов в массиве
+
+	var n int
+	fmt.Scanf("%d", &n)
+	arr := [25]int{}
+	for i := 0; i < n; i++ {
+		var a int
+		fmt.Scanf("%d", &a)
+		arr[i] = a
+		if arr[i] > 0 {
+			fmt.Println(arr[i])
+		}
+	}
+}
+
